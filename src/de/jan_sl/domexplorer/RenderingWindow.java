b1c2faeb26b2478eb98d6e7917d2579117c6390f
PalatinCoder <github@jan-sl.de>
2015-09-24 16:18:05 +0200

    work at RenderingWindow

--- src/de/jan_sl/domexplorer/RenderingWindow.java
+++ src/de/jan_sl/domexplorer/RenderingWindow.java
@@ -3,9 +3,15 @@
  */
 package de.jan_sl.domexplorer;
 
+import java.awt.Color;
 import java.util.Enumeration;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
@@ -15,33 +21,55 @@ import javax.swing.tree.DefaultMutableTreeNode;
  */
 public class RenderingWindow extends JFrame {
 	
+	JScrollPane scrollPane = new JScrollPane();
+	JPanel myContentPane = new JPanel();
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7588087496824177354L;
 
+	DefaultMutableTreeNode bodyNode = null;
+	
 	/**
 	 * create the window
 	 * @param pageTree the page tree from the dom parser
 	 */
 	public RenderingWindow() {
+		this.myContentPane.setLayout(new BoxLayout(myContentPane, BoxLayout.PAGE_AXIS));
+		this.scrollPane.add(this.myContentPane);
+		this.getContentPane().add(this.scrollPane);
 		this.setLocationByPlatform(true);
 		this.setSize(800,600);
 		this.setVisible(false);	
 	}
 	
-	public void render(DefaultMutableTreeNode pageTree) {
+	public void render(DefaultMutableTreeNode pageTree) {	
 		@SuppressWarnings("unchecked")
 		Enumeration<DefaultMutableTreeNode> en = pageTree.preorderEnumeration();
 		
 		while (en.hasMoreElements()) {
-			interpretNode(en.nextElement());
+			processNode(en.nextElement());
 		}
 		
 		this.setVisible(true);
 	}
 
-	private void interpretNode(DefaultMutableTreeNode currentNode) {
+	private void processNode(DefaultMutableTreeNode currentNode) {
+		// grab the title
 		if (currentNode.getLevel() > 0 && currentNode.getParent().toString().equals("title")) this.setTitle(currentNode.toString().trim());
+		
+		// get body node
+		if (currentNode.toString().equals("body")) this.bodyNode = currentNode;
+		
+		// return if we're not inside the body
+		if (bodyNode == null || !bodyNode.isNodeRelated(currentNode)) return;
+		
+		if (currentNode.toString().matches("header|article|h1|h2|h3|h4|nav|div|p")) {
+			JPanel contentPanel = new JPanel();
+			contentPanel.setBorder(new LineBorder(Color.BLACK));
+			contentPanel.add(new JLabel(currentNode.toString()));
+			this.scrollPane.add(contentPanel); // TODO Components werden nicht angezeigt!
+		}
 	}
 }
